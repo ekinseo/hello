7f27d5e230840659756030a933bf0fd94e8f1f70
ekinseo <ekinseo@nate.com>
2017-08-17 13:58:35 +0900

    added say hello

--- manman/src/ssw/ekinseo/Test.java
+++ manman/src/ssw/ekinseo/Test.java
@@ -6,6 +6,7 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Hello again");
     }
     
     public void chageNumber(Number number) {
