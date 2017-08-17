92a5823cdc28d531b64b84c71e035383e2b26327
ekinseo <ekinseo@nate.com>
2017-08-17 14:01:31 +0900

    Third commit

--- manman/src/ssw/ekinseo/Test.java
+++ manman/src/ssw/ekinseo/Test.java
@@ -7,6 +7,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hello again");
+        System.out.println("Hello Second");
     }
     
     public void chageNumber(Number number) {
