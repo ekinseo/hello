d8ada599a331e3d8bd0331203e43546ca00fc05d
ekinseo <ekinseo@nate.com>
2017-08-18 15:15:33 +0900

    fifth Commit

--- manman/src/ssw/ekinseo/GitTest.java
+++ manman/src/ssw/ekinseo/GitTest.java
@@ -1,8 +1,24 @@
 package ssw.ekinseo;
 
 public class GitTest {
-	public int ginName;
-	public String gitURL;
+	private int gitName;
+	private String gitURL;
 	
-
+	public GitTest(int gitName, String gitURL) {
+		this.gitName = gitName;
+		this.gitURL = gitURL;
+	}
+	
+	public void SetGitName(int gitName) {
+		this.gitName = gitName;
+	}
+	public int GetGitName() {
+		return this.gitName;
+	}
+	public void setGitURL(String gitURL) {
+		this.gitURL = gitURL;
+	}
+	public String GetGitURL() {
+		return this.gitURL;
+	}
 }
